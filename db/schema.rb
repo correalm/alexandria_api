d9e8287cf1c3e67412d5a62aa6439d85ec10711c
Luis Miguel Corrêa <89203851+correalm@users.noreply.github.com>
2024-10-13 22:23:49 -0300

    feat: introduce authors

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_10_13_143717) do
+ActiveRecord::Schema.define(version: 2024_10_14_010907) do
+
+  create_table "authors", force: :cascade do |t|
+    t.string "given_name"
+    t.string "family_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "publishers", force: :cascade do |t|
     t.string "name"
