61d3a4c2f99eac6459b40555573b0a67dc44de6b
Luis Miguel Corrêa <89203851+correalm@users.noreply.github.com>
2024-10-14 08:13:03 -0300

    feat(models/author): validate presence of name

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -1,5 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Author, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should validate_presence_of(:given_name) }
+  it { should validate_presence_of(:family_name) }
 end
